24cf55cba08bddab873691ae5be2bc1d2f70e986
vaishnavi <chvkarmy@gmail.com>
2023-02-14 20:34:18 +0530

    Added images and text

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,34 +24,76 @@ export default function Home() {
           ></Image>
           <div className="py-6"></div>
           <div className="justify-center px-6">
-            <h2 className="text-center mainheading font-redhat">
+            <h2 className="mainheading text-center font-redhat">
               Group Chat for Everyone
             </h2>
           </div>
 
           <div className="py-3"></div>
           <div className="justify-center px-6">
-            <p className="text-center text-lightgrey font-redhat subheading">
+            <p className="subheading text-center font-redhat text-lightgrey">
               Meet makes it easy to connect with others face-to-face virtually
               and collaborate across any device.
             </p>
           </div>
-          <div className="px-91px py-4"></div>
+          <div className="py-4"></div>
 
-          <button className="bg-cyan flex gap-1 rounded-full w-48 h-14  ">
-            <p className="text-white font-redhat font-bold text-center pt-4 pl-10 ">
-              Download
-            </p>{" "}
-            <p className="text-lightskyblue font-bold pt-4 ">v1.3</p>
+          <button className=" flex h-14 w-48 flex-row items-center justify-center rounded-full bg-cyan  ">
+            <p className="font-redhat font-bold text-white ">Download</p>
+            <p className="pl-1 font-bold text-lightskyblue ">v1.3</p>
           </button>
 
-          <div className="pt-4 px-118px "></div>
-          <button className="bg-purple rounded-full flex w-139px h-58px opacity-90">
-            <p className="text-white font-redhat font-bold text-center pt-5 pl-8 pr-6">
+          <div className=" pt-4 "></div>
+          <button className="grid h-58px w-139px content-center rounded-full bg-purple opacity-90">
+            <p className=" text-center font-redhat font-bold text-white">
               {" "}
               What is it?
             </p>
           </button>
+
+          <div className="pt-16"></div>
+          <div className="h-20 border border-lightgrey opacity-25"></div>
+          <div className="grid h-14 w-14 content-center rounded-full border border-lightgrey opacity-25">
+            <p className="text-center">01</p>{" "}
+          </div>
+          <div className="pt-16"></div>
+          <div className="grid grid-flow-col grid-rows-2 items-center gap-6">
+            <Image
+              className="rounded-lg"
+              width={150}
+              height={150}
+              src="/assets/desktop/image-woman-in-videocall.jpg"
+              alt="Logo"
+            ></Image>
+            <Image
+              className="rounded-lg"
+              width={150}
+              height={150}
+              src="/assets/desktop/image-women-videochatting.jpg"
+              alt="Logo"
+            ></Image>
+            <Image
+              className="rounded-lg"
+              width={150}
+              height={150}
+              src="/assets/desktop/image-men-in-meeting.jpg"
+              alt="Logo"
+            ></Image>
+            <Image
+              className="rounded-lg"
+              width={150}
+              height={150}
+              src="/assets/desktop/image-man-texting.jpg"
+              alt="Logo"
+            ></Image>
+          </div>
+
+          <div className="pt-16"></div>
+          <p className="font-redhat font-extrabold uppercase tracking-[.25em] text-cyan">
+            Built for modern use
+          </p>
+          <div className="pt-6"></div>
+          <h2 className=""></h2>
         </div>
       </main>
     </>

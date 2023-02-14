98e8843cb8b330496084090a2fabe6693ebb3872
vaishnavi <chvkarmy@gmail.com>
2023-02-14 13:22:33 +0530

    Added font and tailwind

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,14 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import Link from "next/link";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head></Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,18 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      cyan: "rgb(77,150,168)",
+      lightskyblue: "rgb(143,227,249)",
+      purple: "rgb(133,95,177)",
+      lightpurple: "rgb(217,184,255)",
+      grey: "rgb(40,40,61)",
+      lightgrey: "rgb(135,137,157)",
+      white: "rgb(250,250,250)",
+    },
+    fontFamily: {
+      redhat: ["RedHat"],
+    },
     extend: {},
   },
   plugins: [],

4ac208557d5975e83883d124738e3c335586b1b2
vaishnavi <chvkarmy@gmail.com>
2023-02-14 15:02:47 +0530

    Added text csss

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,12 +8,14 @@ export default function Home() {
       </Head>
       <main>
         <h1 className="text-cyan font-redhat">o Morbi in</h1>
-        <p className="text-lightskyblue">Prabhath</p>
+        <p className="text-grey ">Prabhath</p>
         <p className="text-purple">Prabhath</p>
         <p className="text-lightpurple">Prabhath</p>
         <p className="text-grey">Prabhath</p>
         <p className="text-lightgrey">Prabhath</p>
         <p className="text-white">Prabhath</p>
+        <div className="pt-11"></div>
+        <p className="text-grey widetext">Prabhath</p>
       </main>
     </>
   );
